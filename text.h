faa140d573b6e3e32b30131cb9db50a65a5081de
callelj@gmail.com <callelj@gmail.com@845d7c7b-0dab-8951-c7aa-8aee35c4cb88>
2011-08-23 17:05:10 +0000

    Change file trigometry to trigonometry.

--- text.h
+++ text.h
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*
 #include "adc.h"
 #include "delay.h"
 #include "gps.h"
-#include "trigometry.h"
+#include "trigonometry.h"
 
 // Text vars
 static uint16_t const textLines[TEXT_LINES] = {TEXT_TRIG_LINES_LIST};
